f04e1d681bb95fc09f799cb471823a384e5308d5
tahngarth825 <richardlaucla@gmail.com>
2022-01-13 21:47:32 -0800

    adjust numbers to show royals and improve to_s

--- classes/card.rb
+++ classes/card.rb
@@ -4,7 +4,7 @@ class Card
   attr_reader :suit, :number
 
   SUITS   = %w[diamond club heart spade].freeze
-  NUMBERS = Set.new(1..13)
+  NUMBERS = %w[A 2 3 4 5 6 7 8 9 10 J Q K].to_set.freeze
 
   def self.all(random: true)
     result = []
@@ -24,6 +24,6 @@ class Card
   end
 
   def to_s
-    [@suit, @number].to_s
+    "#{@number} of #{@suit}"
   end
 end
