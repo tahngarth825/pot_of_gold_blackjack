c07590d3641331a2bc0d82d35d9a1df497a69ac2
tahngarth825 <richardlaucla@gmail.com>
2022-01-13 22:57:50 -0800

    add helper methods on Hand class

--- classes/hand.rb
+++ classes/hand.rb
@@ -13,28 +13,81 @@ class Hand
   # NOTE: To handle aces, we always add by 11
   # Then, we subtract by 10 per ace if above 21
   def sum
-    total = 0
-    aces = 0
-
-    @cards.each do |card|
-      card_value = value(card.number)
-      total += card_value
-      aces += 1 if card_value == 11
-    end
-
-    while aces.positive? && total > 21
-      aces -= 1
-      total -= 10
-    end
-
-    total
+    sum_with_soft.first
   end
 
   def to_s
     @cards.join(', ')
   end
 
+  def first
+    @cards.first
+  end
+
+  def includes_ace?
+    @cards.any? { |card| card.number == 'A' }
+  end
+
+  def bust?
+    sum > 21
+  end
+
+  def blackjack?
+    sum == 21 && @cards.count == 2
+  end
+
+  def soft17?
+    total, soft = sum_with_soft
+
+    soft && total == 17
+  end
+
+  def can_free_split?
+    can_split? && value(@cards.first.number) != 10
+  end
+
+  def can_split?
+    (@cards.count == 2) && (@cards.first.number == @cards.last.number)
+  end
+
+  def can_free_double?
+    can_double? && [9, 10, 11].include?(sum)
+  end
+
+  def can_double?
+    @cards.count == 2
+  end
+
+  def split
+    # TODO
+  end
+
   private
+
+  def sum_with_soft
+    total = 0
+    aces = 0
+    soft = false # We could also check final aces == original_aces
+
+    @cards.each do |card|
+      card_value = value(card.number)
+
+      total += card_value
+
+      if card_value == 11
+        aces += 1
+        soft = true
+      end
+    end
+
+    while aces.positive? && total > 21
+      soft = false
+      aces -= 1
+      total -= 10
+    end
+
+    [total, soft]
+  end
 
   # NOTE: number can be "A", "2", . . ., "J", "Q", "K"
   def value(number)
