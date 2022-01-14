8659679e862bc3890debc0d769d3ae054c20c081
tahngarth825 <richardlaucla@gmail.com>
2022-01-13 21:44:20 -0800

    add a hand class to handle calculating with aces

---
+++ classes/hand.rb
@@ -0,0 +1,43 @@
+class Hand
+  ROYALS = %w[J Q K].to_set.freeze
+
+  def initialize(cards)
+    @cards = cards
+  end
+
+  def <<(card)
+    @cards << card
+  end
+  alias :add <<
+
+  # NOTE: To handle aces, we always add by 11
+  # Then, we subtract by 10 per ace if above 21
+  def sum
+    total = 0
+    aces = 0
+
+    @cards.each do |card|
+      card_value = value(card.number)
+      total += card_value
+      aces += 1 if card_value == 11
+    end
+
+    while aces.positive? && total > 21
+      aces -= 1
+      total -= 10
+    end
+
+    total
+  end
+
+  # NOTE: number can be "A", "2", . . ., "J", "Q", "K"
+  def value(number)
+    if number == 'A'
+      11
+    elsif ROYALS.include?(number)
+      10
+    else
+      number.to_i
+    end
+  end
+end
